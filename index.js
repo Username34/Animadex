8424ee3fa01779f9cf21bce620d9e2e2738e0166
Mehdi Bouziane <mbouziane@welcome-media.fr>
2018-03-29 14:59:23 +0200

    add ia animal and upload file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var fileUpload = require('express-fileupload');
 var app = express();
+
+// default options
+app.use(fileUpload());
 
 const Rekognition = require('node-rekognition');
 
@@ -7,15 +11,41 @@ const Rekognition = require('node-rekognition');
 const AWSParameters = {
     "accessKeyId": "",
     "secretAccessKey": "",
-    "region": "us-east-2",
+    "region": "",
     "bucket": "",
 }
 
 const rekognition = new Rekognition(AWSParameters);
 
-const imageLabels =  rekognition.detectLabels({Key:'chat.jpg'});
-let test;
-//test = imageLabels.then
-imageLabels.then (function(value) {
-    console.log (value);
+/*app.get('/image', function(req, res) {
+    const imageLabels =  rekognition.detectLabels({Key:'chat.jpg'});
+    imageLabels.then (function(value) {
+        //console.log (value);
+        if (value.Labels[0].Name == 'Animal'){
+            res.send(value.Labels[0].Name);
+        }
+        res.end();
+      });
+});*/
+
+app.post('/upload', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
+   
+    // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+    let sampleFile = req.files.sampleFile;
+   
+    // Use the mv() method to place the file somewhere on your server
+    sampleFile.mv('/Users/mehdi/Documents/Animadex/'+sampleFile.name, function(err) {
+      if (err)
+        return res.status(500).send(err);
+   
+    const s3Images = rekognition.uploadToS3('/**/**/Documents/Animadex/'+sampleFile.name, '/images');
+
+    s3Images.then (function(value) {
+        console.log(value);
+        res.send('File uploaded!');
+      });
+    });
   });
+app.listen(3000);
