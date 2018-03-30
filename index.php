01cae37b00c800c923cd620edb8147f8b81afa74
Tetevemasque <tetevemasque@gmail.com>
2018-03-30 11:25:54 +0200

    "Commit ultime !"

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
   </head>
   <body>
 
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <link rel="stylesheet" href="interface/assets/css/global.css" />
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
 
